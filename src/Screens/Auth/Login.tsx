e2ed53615e960d8ed8491444dc8883edafcdba5d
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2026-03-28 13:22:51 +0100

    Fixing type error

--- src/Screens/Auth/Login.tsx
+++ src/Screens/Auth/Login.tsx
@@ -3,7 +3,7 @@ import { AuthLayout } from "@/Layouts";
 import { loginFormValidation } from "@/Utils/formValidation";
 import { Mail, Lock, RefreshCcw, Loader } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { useAuth } from "@/Hooks";
 const Login = () => {
