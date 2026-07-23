e00ac818813e4d29f5d83ba7a170e87a3ad7a264
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2026-07-23 03:13:34 +0100

    feat: integrate Sentry for error tracking and add app association files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import "./Styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -13,6 +14,13 @@ import NotificationProvider from "./Provider/NotificationProvider.tsx";
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const libraries: "places"[] = ["places"];
+
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
